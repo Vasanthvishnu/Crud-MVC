18a24652152c9650fd96c983f8fbe7f0983d4f64
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-05-31 14:32:19 +0530

    mvc crud model completed

---
+++ Crud-MVC/Models/VehicleModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_MVC.Models
+{
+    public class VehicleModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VehicleNumber { get; set; }
+        public string OwnerName { get; set; }
+        public string DriverName { get; set; }
+        public long ContactNumber { get; set; }
+        public string Location { get; set; }
+    }
+}
