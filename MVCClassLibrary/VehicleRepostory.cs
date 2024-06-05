5487a1038dde45bc21753953f88bf11c7d017200
vasanthvishnu <vasanthraj.selva@gmail.com>
2024-06-05 13:42:29 +0530

    crud mvc

---
+++ MVCClassLibrary/VehicleRepostory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.SqlClient;
+using Crud_MVC;
+
+namespace MVCClassLibrary
+{
+    public class VehicleRepostory
+    {
+        SqlConnection con;
+        IConfiguration _configuration;
+
+        public VehicleRepostory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var pon = _configuration.GetConnectionString("DbConnection");
+            con = new SqlConnection();
+        }
+
+    }
+}
